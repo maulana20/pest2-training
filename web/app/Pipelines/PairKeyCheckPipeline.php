75380239d7e7959403f112f98a763a6798caff38
maulana saputra <maulana.saputra@aqi.co.id>
2023-08-29 16:27:31 +0700

    test pair key pipeline
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

---
+++ web/app/Pipelines/PairKeyCheckPipeline.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Pipelines;
+
+use Closure;
+use App;
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Carbon\Carbon;
+
+class PairKeyCheckPipeline
+{
+    public function __invoke($token, Closure $next): mixed
+    {
+        $decode = $this->decode($token);
+        if (empty($decode)) App::abort(403, "Failed verify");
+        if ($decode->exp < Carbon::now()->timestamp) App::abort(403, "Token expired");
+        if ($decode->feature !== "api") App::abort(403, "Feature api not found");
+        return $next($token);
+    }
+
+    private function decode($token)
+    {
+        try {
+            $publicKey = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . "/../pair.pub");
+            return JWT::decode($token, new Key($publicKey, 'RS256'));
+        } catch (Exception $e) {}
+        return null;
+    }
+}
--- web/tests/Unit/TokenTest.php
+++ web/tests/Unit/TokenTest.php
@@ -2,9 +2,26 @@
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use App\Pipelines\PairKeyCheckPipeline;
 
 it('check pair key', function () {
     $encode = JWT::encode(['feature' => 'token'], file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . "/../pair.sec"), 'RS256');
     $decode = JWT::decode($encode, new Key(file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . "/../pair.pub"), 'RS256'));
     $this->assertEquals($decode->feature, 'token');
+});
+
+it('check pair key with pipeline', function (): void {
+    $token = JWT::encode([
+        'name' => 'Maulana Saputra',
+        'email' => 'maulana.saputra@aqi.co.id',
+        'feature' => 'api',
+        'exp' => 1703983398
+    ], file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . "/../pair.sec"), 'RS256');
+
+    $pipeline = new PairKeyCheckPipeline;
+    $response = $pipeline->__invoke(
+        $token,
+        next: fn () => $token,
+    );
+    expect($response)->toEqual($token);
 });
